e1684e0a9b5483c602e43e1f8119d7c241e246b2
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-11-15 18:49:33 +0530

    Completed Final Page  Redux work

--- src/components/presentation/education.js
+++ src/components/presentation/education.js
@@ -7,11 +7,13 @@ import {skinCodes, fieldCd} from './../../constants/typeCodes';
 // import * as educationActions from '../../actions/educationActions';
 // import {bindActionCreators} from 'redux';
 import { useHistory } from "react-router-dom";
+import {setEducation, updateEducation} from '../../redux/actions/educationActions';
+import { connect } from "react-redux";
 
 function Education(props) {
   console.log('Education');
   let history = useHistory();
-  const [education,setEducation]= useState(props.educationSection);
+  const [education,setEducation]= useState(props.education);
 
   const onchange = (event) => {
     var key =event.target.name;
@@ -25,12 +27,14 @@ function Education(props) {
     return "";
 }
   const onSubmit = async(e) => {
-    //console.log(this.state.educationSection);
-    // if(props.educationSection!=null){
-    //     props.updateEducation(props.document.id,education);
-    // }else{
-    //     props.addEducation(props.document.id,education);
-    // }
+     
+    if(props.education !== null){
+      props.updateEducation(education)
+    }
+    else{
+      props.setEducation(education)
+    }
+
      history.push('/finalize')
   }
 
@@ -90,7 +94,7 @@ function Education(props) {
             </div>
           </div>
           <div className="preview-card">
-            <ResumePreview contactSection={props.contactSection} educationSection={education} skinCd={props?.document?.skinCd}></ResumePreview>            
+            <ResumePreview contactSection={props.contact} educationSection={education} skinCd={props?.document?.skinCd}></ResumePreview>            
           </div>
         </div>
       </div>
@@ -98,9 +102,20 @@ function Education(props) {
   }
 
 
-
+const mapStateToProps = (state) => {
+  return {
+    document : state.document,
+    contact : state.contact,
+    education : state.education
+  }
+}
   
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setEducation : (education) => dispatch(setEducation(education)),
+    updateEducation : (education) => dispatch(updateEducation(education))
+  }
+}
 
-
-export default Education
+export default connect(mapStateToProps, mapDispatchToProps)(Education);
 
