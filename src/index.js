15cfcc3e77d6d3a63809f7e01d96d4a65a4ac05d
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-11-16 10:42:27 +0530

    Integrated redux thunk with firebase

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { composeWithDevTools } from '@redux-devtools/extension';
 import rootReducer from './redux/reducers/rootReducer';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
+import thunk from 'redux-thunk';
+import { reduxFirestore, getFirestore } from 'redux-firestore';
+import { ReactReduxFirebaseProvider, getFirebase } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
 
-const reduxStore = createStore(rootReducer, composeWithDevTools());
+
+const reduxStore = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk).withExtrArgument({getFirebase, getFirestore}), reduxFirestore(firebase)));
 
 const firebaseConfig = {
   apiKey: "AIzaSyDFKDK-gybu5xH_nfnAITnjAzRm3cRLRkY",
@@ -26,11 +31,17 @@ firebase.initializeApp(firebaseConfig);
 firebase.firestore();
 
 ReactDOM.render(
+  <Provider store={reduxStore}>
   <BrowserRouter>
-    <Provider store={reduxStore}>
+    <ReactReduxFirebaseProvider
+      firebase={firebase}
+      config={firebaseConfig}
+      dispatch={reduxStore.dispatch}
+      createFirestoreInstance={createFirestoreInstance}>
       <App />
-    </Provider>
+    </ReactReduxFirebaseProvider>
   </BrowserRouter>
+</Provider>
   ,
   document.getElementById('root')
 );
