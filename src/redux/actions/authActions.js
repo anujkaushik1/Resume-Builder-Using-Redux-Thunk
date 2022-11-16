6576353c0adf30d98696b89220384e88f6f3ea65
Anuj Kaushik <anujkaushik1512@gmail.com>
2022-11-16 12:04:57 +0530

    Created Action Creator functions for signin, signup & signout

--- src/redux/actions/authActions.js
+++ src/redux/actions/authActions.js
@@ -2,49 +2,51 @@ import { getFirebase } from 'react-redux-firebase';
 import { getFirestore } from 'redux-firestore';
 import * as authActions from './action';
 
- const registerReq = () => {
-    return {
-        type : authActions.SIGN_UP_REQUEST
-    } 
-}
+// ACTION CREATOR FUNCTIONS 
 
- const registerFail = (err) => {
+const registerReq = () => {
     return {
-        type : authActions.SIGN_UP_FAILED,
-        payload : err.message
+        type: authActions.SIGN_UP_REQUEST
     }
 }
 
- const registerSuc = () => {
+const registerFail = (err) => {
     return {
-        type : authActions.SIGN_UP_SUCCESS
+        type: authActions.SIGN_UP_FAILED,
+        payload: err.message
+    }
+}
+
+const registerSuc = () => {
+    return {
+        type: authActions.SIGN_UP_SUCCESS
     }
 }
 
 const removeError = () => {
     return {
-        type : authActions.REMOVE_ERROR
+        type: authActions.REMOVE_ERROR
     }
 }
 
 export const register = (userData) => {  // userdata will contain email & password
-    return (dispatch, getState, {getFirebase, getFirestore}) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
 
         dispatch(registerReq()) // request chl rhi hai wait (loading = true)
 
         const firebase = getFirebase();  // thunk mei firebase aagaya
-        const firestore = getFirebase(); 
+        const firestore = getFirebase();
 
-        firebase.auth().createUserWithEmailAndPassword(userData.email, userData.password).then(async(data)=> {
+        firebase.auth().createUserWithEmailAndPassword(userData.email, userData.password).then(async (data) => {
             const res = firestore.collection('users').doc(data.user.uid).set({
-                email : userData.email,
-                resumeIds : []
+                email: userData.email,
+                resumeIds: []
             });
             //success =>
 
             dispatch(registerSuc());
 
-        }).catch((err)=> {
+        }).catch((err) => {
             dispatch(registerFail(err));
 
             setTimeout(() => {
@@ -52,5 +54,67 @@ export const register = (userData) => {  // userdata will contain email & passwo
             }, 2000);
         })
 
+    }
+}
+
+const signInReq = () => {
+    return {
+        type: authActions.SIGN_IN_REQUEST
+    }
+}
+
+const signInFail = (err) => {
+    return {
+        type: authActions.signInFail,
+        payload: err.message
+    }
+}
+
+const signInSuc = () => {
+    return {
+        type: authActions.SIGN_IN_SUCCESS
+    }
+}
+
+
+
+export const signIn = (userData) => {  // userdata will contain email & password
+    return async (dispatch, getState, { getFirebase, getFirestore }) => {
+
+        dispatch(signInReq()) // request chl rhi hai wait (loading = true)
+
+        const firebase = getFirebase();  // thunk mei firebase aagaya 
+
+        try {
+            const res = await firebase.auth().signInWithEmailAndPassword(userData.email, userData.password);
+
+            // success =>
+
+            dispatch(signInSuc())
+
+        } catch (error) {
+            dispatch(signInFail(error))
+
+            setTimeout(() => {
+                dispatch(removeError());
+            }, 2000);
+        }
+
+    }
+}
+
+// direct work for signout
+
+export const signOut = () => {
+    return (dispatch, { getFirebase, getFirestore }) => {
+        const firebase = getFirebase();
+        firebase.auth().signOut().then(() => {  // signout => predefined function
+            dispatch({ type: authActions.SIGN_OUT_SUCCESS })
+        }).catch((err) => {
+            dispatch({type : authActions.SIGN_OUT_FAILED, payload : err});
+            setTimeout(() => {
+                dispatch(removeError());
+            }, 2000);
+        })
     }
 }
